faddab9bf550d2ad52cd891b06e480fceddaeca6
Asiri H <asiriofficial@gmail.com>
2025-12-29 17:10:02 +0000

    Cannot destructure property 'getFieldState' of '(0 , {imported module [p

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -44,15 +44,30 @@ const FormField = <
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext)
   const itemContext = React.useContext(FormItemContext)
-  const { getFieldState, formState } = useFormContext()
-
-  const fieldState = getFieldState(fieldContext.name, formState)
+  const form = useFormContext()
 
   if (!fieldContext) {
     throw new Error("useFormField should be used within <FormField>")
   }
 
   const { id } = itemContext
+  
+  if (!form || !fieldContext.name) {
+    return {
+      id,
+      name: fieldContext.name,
+      formItemId: `${id}-form-item`,
+      formDescriptionId: `${id}-form-item-description`,
+      formMessageId: `${id}-form-item-message`,
+      error: null,
+      invalid: false,
+      isDirty: false,
+      isTouched: false,
+    };
+  }
+
+  const { getFieldState, formState } = form;
+  const fieldState = getFieldState(fieldContext.name, formState)
 
   return {
     id,
